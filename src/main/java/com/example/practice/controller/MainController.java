68b80cab5e1c5b743dc54066a5ccb8d8ceff4e21
gtithathonor <jeong.suyoung92@gmail.com>
2022-08-22 19:35:48 +0900

    사이드 프로젝트 연습중 - 부트스트랩으로 view 만들기

---
+++ src/main/java/com/example/practice/controller/MainController.java
@@ -0,0 +1,15 @@
+package com.example.practice.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@AllArgsConstructor
+public class MainController {
+
+    @GetMapping("/")
+    public String go() {
+        return "index";
+    }
+}
